14c7a6a9d6a02a1eb232791221a8cf3e56871589
mathew35 <matus549@gmail.com>
2022-11-27 19:18:06 +0100

    Pracovanie na profile

--- content.php
+++ content.php
@@ -1,13 +1,99 @@
 <?php
 
-    if(isset($_GET['detail'])){
-        include 'productdetail.php';
-    }
-    if (isset($_SESSION['farmer'])) {
-        include 'farmer_view.php';
-    }
-    else{
-        include 'main_view.php';
-    }
+if (isset($_GET['detail'])) {
+    include 'productdetail.php';
+}
+if (isset($_SESSION['farmer'])) {
+    include 'farmer_view.php';
+} else {
+    include 'main_view.php';
+}
 
 // todo
+?>
+<div id="tableItems">
+    <div class="tableItem" id="added">
+        <p>added</p>
+        <p>0 Kč / 0</p>
+        <p></p>
+        <p><a href="index3.php?detail=2"><button id="myBtn">Detail</button></a></p>
+    </div>
+    <div class="tableItem" id="added">
+        <p>added</p>
+        <p>0 Kč / 0</p>
+        <p></p>
+        <p><a href="index3.php?detail=3"><button id="myBtn">Detail</button></a></p>
+    </div>
+    <div class="tableItem" id="added">
+        <p>added</p>
+        <p>0 Kč / 0</p>
+        <p></p>
+        <p><a href="index3.php?detail=4"><button id="myBtn">Detail</button></a></p>
+    </div>
+    <div class="tableItem" id="added">
+        <p>added</p>
+        <p>0 Kč / 0</p>
+        <p></p>
+        <p><a href="index3.php?detail=5"><button id="myBtn">Detail</button></a></p>
+    </div>
+    <div class="tableItem" id="added">
+        <p>added</p>
+        <p>0 Kč / 0</p>
+        <p></p>
+        <p><a href="index3.php?detail=6"><button id="myBtn">Detail</button></a></p>
+    </div>
+    <div class="tableItem" id="added">
+        <p>added</p>
+        <p>0 Kč / 0</p>
+        <p></p>
+        <p><a href="index3.php?detail=7"><button id="myBtn">Detail</button></a></p>
+    </div>
+    <div class="tableItem" id="added">
+        <p>added</p>
+        <p>0 Kč / 0</p>
+        <p></p>
+        <p><a href="index3.php?detail=8"><button id="myBtn">Detail</button></a></p>
+    </div>
+    <div class="tableItem" id="added">
+        <p>added</p>
+        <p>0 Kč / 0</p>
+        <p></p>
+        <p><a href="index3.php?detail=9"><button id="myBtn">Detail</button></a></p>
+    </div>
+    <div class="tableItem" id="added">
+        <p>added</p>
+        <p>0 Kč / 0</p>
+        <p></p>
+        <p><a href="index3.php?detail=10"><button id="myBtn">Detail</button></a></p>
+    </div>
+    <div class="tableItem" id="added">
+        <p>added</p>
+        <p>0 Kč / 0</p>
+        <p></p>
+        <p><a href="index3.php?detail=11"><button id="myBtn">Detail</button></a></p>
+    </div>
+    <div class="tableItem" id="added">
+        <p>added</p>
+        <p>0 Kč / 0</p>
+        <p></p>
+        <p><a href="index3.php?detail=12"><button id="myBtn">Detail</button></a></p>
+    </div>
+    <div class="tableItem" id="added">
+        <p>added</p>
+        <p>0 Kč / 0</p>
+        <p></p>
+        <p><a href="index3.php?detail=13"><button id="myBtn">Detail</button></a></p>
+    </div>
+    <div class="tableItem" id="">
+        <p></p>
+        <p>0 Kč / 0</p>
+        <p></p>
+        <p><a href="index3.php?detail=14"><button id="myBtn">Detail</button></a></p>
+    </div>
+    <div class="tableItem" id="added">
+        <p>added</p>
+        <p>0 Kč / 0</p>
+        <p></p>
+        <p><a href="index3.php?detail=15"><button id="myBtn">Detail</button></a></p>
+    </div>
+</div>
---
+++ get_acc.php
@@ -0,0 +1,19 @@
+<?php
+require "services.php";
+session_start();
+$db = new AccountService();
+$user = $db->get('ACCOUNTS', '*', "(LOGIN='" . $_SESSION['user'] . "')");
+$arr = $user->fetch();
+for ($i = 0; $i < $user->rowCount(); $i++) {
+    for ($j = 3; $j < 4; $j++) {
+        echo $arr[$j] . ",";
+    }
+}
+
+$user = $db->get('FARMERS', '*', "(LOGIN='" . $_SESSION['user'] . "')");
+$arr = $user->fetch();
+for ($i = 0; $i < $user->rowCount(); $i++) {
+    for ($j = 1; $j < 4; $j++) {
+        echo $arr[$j] . ",";
+    }
+}
