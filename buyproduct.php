f4b99312426aff67ba0eb207a69e52cfe0bd7faa
ElinkaFIT <alena.klimecka@gmail.com>
2022-11-27 18:45:13 +0100

    pridat do kosiku vice polozek

--- buyproduct.php
+++ buyproduct.php
@@ -8,11 +8,18 @@
 
         $getcid = $db->get("SHOPPING_CART", "CARTID", "USER='" . $user . "'");
         $cid = $getcid->fetch();
-        
 
-        $db->add("CART_CROP", "('".$cid[0]."','".$pid."','".$amount."')");
+        $basketin = $db->get("CART_CROP", "AMOUNT", "CROPID='" . $pid . "'");
 
-        echo "Uživatel " . $user . " přidal " . $amount . " kusů produktu " . $pid;
+        if ($basketin->rowCount() != 0){
+            $basket = $basketin->fetch();
+            $amount = $amount + $basket[0];
+            $db->update("CART_CROP", "AMOUNT='".$amount. "'", "CROPID='".$pid."'"); 
+        }
+        else{
+            $db->add("CART_CROP", "('".$cid[0]."','".$pid."','".$amount."')"); 
+        }
+        echo "Uživatel " . $user . " má v košíku " . $amount . " kusů produktu " . $pid;
     }
 
     if ($_POST['paramount'] == NULL){
