4287a9f2fde7918ddb68c292ccdb2ff176280762
mathew35 <matus549@gmail.com>
2023-01-08 22:23:59 +0100

    Uprava samozberu

--- src/php_script/joinharvest.php
+++ src/php_script/joinharvest.php
@@ -1,47 +1,40 @@
-<?php 
+<?php
 // Author: Alena Klimecká
 
-    require "../php_ajax/services.php";
-    
-    function joinharv($data){
-        session_start();
-        $db = new AccountService();
-        $eid = $data;
+require "../php_ajax/services.php";
 
-        if(isset($_SESSION['user'])){
-            $user = $_SESSION['user']; 
-            $db->add("HARVEST_EVENT_ATTENDANTS", "('".$eid."','".$user."')");
-            echo "Uživatel " . $user . " byl úspěšně přihlášen na samosběr " . $eid . ".";
-        }
-        else{
-            echo "Je nutné se nejprve přihlásit.";
-        }
+function joinharv($data)
+{
+    session_start();
+    $db = new AccountService();
+    $eid = $data;
+
+    if (isset($_SESSION['user'])) {
+        $user = $_SESSION['user'];
+        $db->add("HARVEST_EVENT_ATTENDANTS", "('" . $eid . "','" . $user . "')");
+        echo "Uživatel " . $user . " byl úspěšně přihlášen na samosběr " . $eid . ".";
+    } else {
+        echo "Je nutné se nejprve přihlásit.";
     }
+}
 
-    function leaveharv($data){
-            session_start();
-            $db = new AccountService();
-            $eid = $data;
+function leaveharv($data)
+{
+    session_start();
+    $db = new AccountService();
+    $eid = $data;
 
-            if(isset($_SESSION['user'])){
-                $user = $_SESSION['user']; 
-                $db->remove("HARVEST_EVENT_ATTENDANTS", " EVENTID = '{$eid}' AND LOGIN = '{$user}'" );
-                echo "Uživatel " . $user . " byl úspěšně odhlášen ze samosběru " . $eid . ".";
-            }
-            else{
-                echo "Je nutné se nejprve přihlásit.";
-            }
-        }
-
-        if (isset($_POST['param1'])) {
-            joinharv($_POST['param1']);
-        }
-
-
-
-    if (isset($_POST['param1'])) {
-        joinharv($_POST['param1']);
+    if (isset($_SESSION['user'])) {
+        $user = $_SESSION['user'];
+        $db->remove("HARVEST_EVENT_ATTENDANTS", " EVENTID = '{$eid}' AND LOGIN = '{$user}'");
+        echo "Uživatel " . $user . " byl úspěšně odhlášen ze samosběru " . $eid . ".";
+    } else {
+        echo "Je nutné se nejprve přihlásit.";
     }
-    elseif(isset($_POST['param2'])) {
-        leaveharv($_POST['param2']);
-    }
+}
+
+if (isset($_POST['param1'])) {
+    joinharv($_POST['param1']);
+} elseif (isset($_POST['param2'])) {
+    leaveharv($_POST['param2']);
+}
